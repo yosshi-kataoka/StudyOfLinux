baf1aef23cc5144dbf1699158f740bfde7d2c8d7
github yosshi-kataoka <github>
2024-12-29 06:36:33 +0900

    閲覧数の最も多い記事のﾄﾞﾒｲﾝ名、ﾍﾟｰｼﾞﾀｲﾄﾙ、総閲覧数を表示できるようにしました。

--- src/src/tests/LogAnalysesTest.php
+++ src/src/tests/LogAnalysesTest.php
@@ -4,46 +4,99 @@ namespace WikiLogs\Tests;
 
 use PHPUnit\Framework\TestCase;
 use WikiLogs\LogAnalyses;
+use WikiLogs\ViewCountProcessor;
+use WikiLogs\DomainViewProcessor;
 use Dotenv;
 use PDO;
+use ReflectionClass;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once(__DIR__ . '/../lib/LogAnalyses.php');
+require_once(__DIR__ . '/../lib/ViewCountProcessor.php');
+require_once(__DIR__ . '/../lib/DomainViewProcessor.php');
 
 class LogAnalysesTest extends TestCase
 {
+  // PDOオブジェクトを格納するプロパティ
+  private $pdo;
+  // LogAnalysesオブジェクトを格納するプロパティ
+  private $logAnalyses;
+
   public function setUp(): void
   {
+    parent::setUp();
     // .envファイルのロード
     $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__) . '/lib');
     $dotenv->load();
+    // PDOモックオブジェクトの作成
+    $this->pdo = $this->createMock(PDO::class);
+    $this->logAnalyses = new LogAnalyses();
   }
   public function testStart()
   {
-    $logAnalyses = new LogAnalyses();
-    $this->assertSame(1, $logAnalyses->start());
+    $this->assertTrue($this->logAnalyses->start());
   }
 
-
+  // 下記テストでは、モックを使用しているため、ConnectDb()のアクセス権をprotectedに変更する必要がある
   public function testConnectDb()
   {
-    // PDOオブジェクトのモックを作成
-    $pdoMock = $this->createMock(PDO::class);
-
-    // PDOモックにメソッドの返り値を設定
-    $pdoMock->method('setAttribute')->willReturn(true);
-
-    // クラスのインスタンス化
-    $database = $this->getMockBuilder(LogAnalyses::class)
-      ->onlyMethods(['connectDb'])
-      ->getMock();
-
+    // LogAnalysesオブジェクトの作成
+    $logAnalyses = $this->getMockBuilder(LogAnalyses::class)->onlyMethods(['connectDb'])->getMock();
     // connectDbメソッドのモック設定
-    $database->expects($this->once())
-      ->method('connectDb')
-      ->willReturn($pdoMock);
+    $logAnalyses->method('connectDb')->willReturn($this->pdo);
+    $reflection = new ReflectionClass($logAnalyses);
+    $method = $reflection->getMethod('connectDb');
+    $method->setAccessible(true);
+    // privateメソッドのテスト
+    $pdo = $method->invoke($logAnalyses);
+    $this->assertInstanceOf(PDO::class, $pdo);
+  }
 
-    // 実際にconnectDbメソッドを呼び出してテスト
-    $this->assertSame($pdoMock, $database->connectDb());
+  public function testSelectNumber()
+  {
+    $reflection = new ReflectionClass($this->logAnalyses);
+    $method = $reflection->getMethod('selectNumber');
+    $method->setAccessible(true);
+    // 標準入力iを入力
+    $inputNumber = $method->invoke($this->logAnalyses);
+    $this->assertSame(1, $inputNumber);
+    //　標準入力2を入力
+    $inputNumber = $method->invoke($this->logAnalyses);
+    $this->assertSame(2, $inputNumber);
+  }
+
+  public function testIsNotFloat()
+  {
+    $reflection = new ReflectionClass($this->logAnalyses);
+    $method = $reflection->getMethod('isNotFloat');
+    $method->setAccessible(true);
+    $output = $method->invoke($this->logAnalyses, 1);
+    $this->assertTrue($output);
+    $output = $method->invoke($this->logAnalyses, 1.5);
+    $this->assertFalse($output);
+  }
+
+  public function testIsOneOrTwo()
+  {
+    $reflection = new ReflectionClass($this->logAnalyses);
+    $method = $reflection->getMethod('isOneOrTwo');
+    $method->setAccessible(true);
+    $output = $method->invoke($this->logAnalyses, 1);
+    $this->assertTrue($output);
+    $output = $method->invoke($this->logAnalyses, 2);
+    $this->assertTrue($output);
+    $output = $method->invoke($this->logAnalyses, 3);
+    $this->assertFalse($output);
+  }
+
+  public function testGetProcessor()
+  {
+    $reflection = new ReflectionClass($this->logAnalyses);
+    $method = $reflection->getMethod('getProcessor');
+    $method->setAccessible(true);
+    $output = $method->invoke($this->logAnalyses, 1);
+    $this->assertSame(ViewCountProcessor::class, get_class($output));
+    $output = $method->invoke($this->logAnalyses, 2);
+    $this->assertSame(DomainViewProcessor::class, get_class($output));
   }
 }
